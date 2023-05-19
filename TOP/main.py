f80de9fe89171e549b861eb1ab29a1253929e14d
Ринат Мухитов <malayka_73@inbox.ru>
2023-05-19 21:12:09 +0400

    Usloviya

---
+++ TOP/main.py
@@ -0,0 +1,72 @@
+﻿#int = 1 # целые числа (int)
+#type_float = 1.23 # числа с точкой
+#type_str = "Строки пишутся в кавычках"
+#type_bool_one = True # булевы значения истина(правда) bool()
+#type_bool_two = False # булевы значения ложь
+#type_none = None
+
+# a = 123
+# print(a * 2, type(a)) #int
+# a = str(a) # str
+# print(a * 2, type(a)) #str
+# a = float(a) #float
+# print(a * 2, type(a))
+
+# b = "123"
+# print(b, type(b))
+# b = int(b)
+# print(b, type(b))
+
+# z = (input("Введите число - мы умножим его на 2: "))  # input() - по умолчанию является строкой
+# z = int(z) # из строки сделали число
+# print(z * 2)
+
+# print("Заполните информацию:")
+# Name = input("Введите имя: ")
+# Surname = input("Введите фамилию: ")
+# Аge = input("Введите свой возраст: ")
+# Job = input("Место работы: ")
+# print("Информация о пользователе: ")
+# print(Name)
+# print(Surname)
+# print(Age)
+# print(Job)
+
+# myName = "Rinat"
+# print("привет" + " " + myName)
+# print("привет" + " " + "мир!")
+# print(f"привет {}")
+# print("привет"," ","мир!")
+
+# x = int(input("введите число: "))
+# print(x // 10)
+# print(x % 10)
+
+# a = 3
+# b = 2
+# h = a / b * 10
+
+# print(h)
+# print(type(h))
+
+# x = int(input("Введите число:"))
+# x1 = x % 10
+# x2 = x // 10
+# x3 = x2 % 10
+# x4 = x2 // 10
+# x5 = x4 % 10
+# x6 = x1 + x3 + x5
+# print(x1)
+# print(x3)
+# print(x5)
+# print(x6)
+
+# x = int(input("Введите число 1:" ))
+# y = int(input("Введите число 2:"))
+# print(f"{x}{y}")
+
+# x = int(input("Введите градус Цельсия:" ))
+# y = 9 / 5 * x + 32
+# print("Градусы Фаренгейта")
+# print(y)
+
