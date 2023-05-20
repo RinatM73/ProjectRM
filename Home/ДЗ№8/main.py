0f72640774d196e84bc3cc6af538871d3c57ac87
Ринат Мухитов <malayka_73@inbox.ru>
2023-05-20 22:20:37 +0400

    DZ1

---
+++ Home/ДЗ№8/main.py
@@ -0,0 +1,35 @@
+# Задание 1
+
+# x = int(input("Введите число №1: "))
+# y = int(input("Введите число №2: "))
+# z = int(input("Введите число №3: "))
+# print("Сумма чисел: ")
+# print(x + y + z)
+# print("Произведение чисел: ")
+# print(x * y * z)
+
+# Задание 2
+
+# x = int(input("Зарплата за месяц: "))
+# y = int(input("Сумма месячного платежа по кредиту: "))
+# z = int(input("Задолженность за коммунальные услуги: "))
+# print("Остаток зарплаты: ")
+# print(x - y - z)
+
+# Задание 3
+
+# d1 = int(input("Введите длину диагонали d1, см: "))
+# d2 = int(input("Введите длину диагонали d2, см: "))
+# print("Площадь ромба, см2")
+# print(d1 * d2 / 2)
+
+# Задание 4
+
+# print("to be\nor not\nto be")
+
+# Задание 5
+
+print('"Life is what happens')
+print("      when")
+print("          you’re busy making other plans”")
+print("                                      John Lennon.")
