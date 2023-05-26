7e531b14ff34d412cef73f3e6a74b08e5c1e0df7
Ринат Мухитов <malayka_73@inbox.ru>
2023-05-26 21:17:48 +0400

    ForWhile

---
+++ TOP/main3.py
@@ -0,0 +1,92 @@
+# myName = "Rinat"
+# start = int(input("Введите начальное значение: "))
+# end = int(input("Введите конечное значение: "))
+# for i in range(0,29,2):
+#     if i % 2 != 0:
+#         print(i)
+
+# for i in myName:
+#     print(i)
+#     if i == "n":
+#         break
+
+# a = 2
+# b = 3
+# if a + b == 5:
+#     for i in range(0,10):
+#         print((a+b)*i)
+
+# x = int(input("Введите число на проверку(сложное или простое): "))
+# for i in range(2,x+1):
+#     print(f"делим число {x} на {i}")
+#     print(f"Остаток {x % i}")
+#     if x % i == 0 and x != i:
+#         print("Число сложное")
+#         break
+#     elif i == x:
+#         print("Число простое")
+
+# for i in range (0,100):
+#     if i % 6 == 0 and i % 2 == 0:
+#         print(i)
+
+
+# x = int(input("Введите число: "))
+# for i in range(1,6):
+#     print(f"{x} * {i} = {x * i}")
+
+# sum = 0
+# for i in range(5,8):
+#     sum = sum + i
+#     print(sum)
+
+# i = 0
+# b = 10
+# while i < 10 and b > 0:
+#     print(i)
+#     print(b)
+#     i = i + 1
+#     b = b - 3
+
+# i = 0
+# b = 10
+# while i != 11 and i < 100:
+#     print(i)
+#     i = i + 2
+
+# i = 0
+# b = 10
+# print("Первый цикл")
+# while True:
+#     print(12)
+#     i = i + 1
+#     if i > 10:
+#         break
+# print("Второй цикл")
+# x = True
+# while x == True:
+#     print(i)
+#     i = i + 1
+#     if i > 15:
+#         x = False
+
+# myName = "fff"
+# print(len(myName))
+# i = 0
+# while i < len(myName):
+#     print(i)
+#     i+=1
+
+x = int(input("Введите число: "))
+i = x
+while x > 0:
+    i = i - 1
+    x = x * i
+    print(x)
+
+# sum = 1
+# x = int(input("Введите число: "))
+# for i in range(1,x+1):
+#     sum = sum * i
+#     print(sum)
+
