b4da74c8d9a557f2d478fbb5b3d3708c2f4dbbc5
Ринат Мухитов <malayka_73@inbox.ru>
2023-06-22 23:49:10 +0400

    DZ

--- Home/DZ15/main.py
+++ Home/DZ15/main.py
@@ -1,10 +1,10 @@
 print("Регистрация гостей")
-guestList = []
+guestList = [] #создали пустой массив
 while True:
     if len(guestList) <= 5: # если гостей до 5 включительно задаем три действия
-        vybor = input("Выберите действие\n1 - Добавить гостя\n2 - Удалить гостя\n3 - Просмотреть список гостей\n> ")
+        vybor = input("Выберите действие\n1 - Добавить гостя\n2 - Удалить гостя\n3 - Просмотреть список гостей\n> \n")
     else: # если гостей больше 5 задаем четыре действия
-        vybor = input("Выберите действие\n1 - Добавить гостя\n2 - Удалить гостя\n3 - Просмотреть список гостей\n4 - Закончить приглашение?\n> ")
+        vybor = input("Выберите действие\n1 - Добавить гостя\n2 - Удалить гостя\n3 - Просмотреть список гостей\n4 - Закончить приглашение?\n> \n")
     if vybor == "1":
         if len(guestList) < 10: # максимальное количество гостей 10
             guestName = input("Введите имя гостя: ")
@@ -12,18 +12,12 @@ while True:
             infoGuest = {
                 "guestName" : guestName,
                 "guestAge" : guestAge,
-                "guestReb" : ""
-            }
-            if guestAge <= 10:
+            }     # создали объект
+            if guestAge <= 10:  # создали условие при котором нельзя приглашать гостей
                 print("Нельзя приглашать гостей младше 10 лет включительно!\n")
             else:
-                guestList.append(infoGuest)
+                guestList.append(infoGuest) # в противном случае гость добавляется в список
                 print("Гость добавлен\n")
-            for i in range(0,len(guestList)):
-                if guestAge < 18:
-                    guestList[i] = x
-                else:
-                    guestList[i] = y
         else:
             print("Список гостей заполнен!")
     elif vybor == "2":
@@ -36,12 +30,22 @@ while True:
             print("Список пуст!")
     elif vybor == "3":
         if len(guestList) > 0:
-            textGuest = ""
+            textGuest = "" # создали пустую переменную для "взрослых"
+            textGuest1 = "" # создали пустую переменную для "детей"
             print("Список гостей: ")
-            for i in range(0,len(guestList)): #создаем цикл для просмотра списка гостей
-                textGuest += f"{i + 1} - Имя: {guestList[i]['guestName']}\n    Возраст: {guestList[i]['guestAge']}\n" 
-                # плюсуем к i единицу для того чтобы в выведенном списке отсчет начинался от 1, а не от 0
-            print(textGuest) # вывод списка гостей если он не пуст
+            n = 0 # создали счетчик для нумерации гостей
+            for i in range(0,len(guestList)): #создаем цикл для просмотра списка гостей("Взрослые")
+                if guestList[i]['guestAge'] >= 18:
+                    n = n + 1 # добавляем единицу к порядковому номеру каждого следующего гостя
+                    textGuest += f"{n} - Имя: {guestList[i]['guestName']}\n    Возраст: {guestList[i]['guestAge']}\n" 
+            print("Взрослые:\n")
+            print(textGuest) # вывод списка гостей("Взрослые") если он не пуст
+            for i in range(0,len(guestList)): #создаем цикл для просмотра списка гостей("Дети")
+                if guestList[i]['guestAge'] < 18:
+                    n = n + 1 # добавляем единицу к порядковому номеру каждого следующего гостя
+                    textGuest1 += f"{n} - Имя: {guestList[i]['guestName']}\n    Возраст: {guestList[i]['guestAge']}\n" 
+            print("Дети:\n")
+            print(textGuest1) # вывод списка гостей("Дети") если он не пуст
         else:
             print("Список пуст!")
     elif vybor == "4":
