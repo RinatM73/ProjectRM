5ffef7f27be39568be4113fd58f7dbe42dbe73c7
Ринат Мухитов <malayka_73@inbox.ru>
2023-07-01 15:37:27 +0400

    DZ

--- Home/DZ18,19/main.py
+++ Home/DZ18,19/main.py
@@ -1,16 +1,16 @@
-# # Задание 1
+#  Задание 1
 
-# numberList = [1,2,3,4,5,6,7,8,9,22,11,10,6,5,22]
-# def delNumber(massiv):
-#     print("вошел", massiv, "кол-во",len(massiv) )
-#     for i in range(0,len(massiv)):
-#         if i == len(massiv):
-#             return massiv
+# numberList = [1,2,3,4,5,6,7,8,9,22,11,10,6,5,22] # создаем список
+# def delNumber(massiv): # cоздаем функцию
+#     print("вошел", massiv, "кол-во",len(massiv) ) # выводим список чисел и их количество
+#     for i in range(0,len(massiv)): # создаем цикл для списка чисел
+#         if i == len(massiv): 
+#             return massiv   # проверяем весь список чисел и возвращяем их значения
 #         elif massiv[i] % 2 == 0 :
-#             massiv.pop(i)
-#             print("вышел", massiv)
-#             delNumber(massiv)
-# delNumber(numberList)
+#             massiv.pop(i)   # удаляем четное число
+#             print("вышел", massiv) # выводим список чисел после удаления нечетного числа
+#             delNumber(massiv) # возвращаемся к началу функции
+# delNumber(numberList) # задаем в качестве аргумента функции список чисел и запускаем её
 
 
 # Задание 2
@@ -54,7 +54,7 @@ def neopl(massiv):
             print("Список обновлен")
             print("Количество: ", len(massiv))
             for i in range(0,len(massiv)):
-                print(f"Имя {clientList[i]['myName']} | Статус {clientList[i]['status']}")
+                print(f"Имя: {clientList[i]['myName']} | Статус: {clientList[i]['status']}")
             print("---------------------------------")
             neopl(massiv)
 neopl(clientList)
