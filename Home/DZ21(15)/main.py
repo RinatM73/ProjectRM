a2f704bd24ea1aeb164e75fbdd675d9e692823b7
Ринат Мухитов <malayka_73@inbox.ru>
2023-07-06 18:52:04 +0400

    DZ

--- Home/DZ21(15)/main.py
+++ Home/DZ21(15)/main.py
@@ -11,6 +11,7 @@ class BlockPit:
     def __init__(self, form, power):
         self.form = form
         self.power = power
+
     def start(self):
         print("Запуск блока питания")
 
@@ -21,8 +22,40 @@ class MatPlata:
     def __init__(self, tip, marka):
         self.tip = tip
         self.marka = marka
+
     def start(self):
         print("Запуск материнской платы")
+
+    def stop(self):
+        print("Стоп")
+
+class Proc:
+    def __init__(self, yadro, chast):
+        self.yadro = yadro
+        self.chast = chast
+
+    def start(self):
+        print("Запуск процессора")
+
+    def stop(self):
+        print("Стоп") 
+
+class Operpam:
+    def __init__(self, pamyat, marka):
+        self.pamyat = pamyat
+        self.marka = marka
+
+    def start(self):
+        print("Запуск процессора")
+
+    def stop(self):
+        print("Стоп")
+
+class Vidcard:
+    def __init__(self, pamyat, marka)
+
+    def start(self):
+        print("Запуск видеокарты")
 
     def stop(self):
         print("Стоп")
