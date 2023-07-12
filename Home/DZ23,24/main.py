c4d46333b8d6140f28e242ba357a41e181927f23
Ринат Мухитов <malayka_73@inbox.ru>
2023-07-12 19:28:26 +0400

    DZ

--- Home/DZ23,24/main.py
+++ Home/DZ23,24/main.py
@@ -77,7 +77,7 @@ class Moderator(User):
     def blocking_user(self, users_list):
         text_user_list = f"id | first_name | blocking | status \n"
         for i in range(0,len(users_list)):
-            text_user_list += f"{users_list[i].user_id}  {users_list[i].first_name}  {users_list[i].blocking} {users_list[i].status}\n"
+            text_user_list += f"{users_list[i].user_id}    {users_list[i].first_name}         {users_list[i].blocking}      {users_list[i].status}\n"
         print(text_user_list)
         input_user_id = int(input("Введите id пользователя для блокировки"))
         for i in range(0,len(users_list)):
