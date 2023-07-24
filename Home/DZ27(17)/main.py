bb6222dd7c1670b117663d6bceaf9fd9fef132a0
Ринат Мухитов <malayka_73@inbox.ru>
2023-07-24 14:44:12 +0400

    DZ

--- Home/DZ27(17)/main.py
+++ Home/DZ27(17)/main.py
@@ -1,44 +1,12 @@
-print("Регистрация гостей")
-guestList = []
-blackList = ["Костя", "Федя", "Петя"]
-while True:
-    if len(guestList) <= 5: # если гостей до 5 включительно задаем три действия
-        vybor = input("Выберите действие\n1 - Добавить гостя\n2 - Удалить гостя\n3 - Просмотреть список гостей\n> ")
-    else: # если гостей больше 5 задаем четыре действия
-        vybor = input("Выберите действие\n1 - Добавить гостя\n2 - Удалить гостя\n3 - Просмотреть список гостей\n4 - Закончить приглашение?\n> ")
-    if vybor == "1":
-        if len(guestList) < 10: # максимальное количество гостей 10
-            guest_name = input("Введите имя: ")
-            if guest_name not in blackList: # если введенное имя не присутствует в черном списке, то добавляем его в список гостей
-                guestList.append(guest_name) # добавление введенного имени в список гостей
-            else: # в противном случай имя не добавляется
-                print("Данный гость в черном списке!")
-        else:
-            print("Список гостей заполнен!")
-    elif vybor == "2":
-        if len(guestList) > 0:
-            guest_name = input("Введите имя: ")
-            guestList.remove(guest_name) # удаление введенного имени из списка гостей если список не пуст
-        else:
-            print("Список пуст!")
-    elif vybor == "3":
-        if len(guestList) > 0:
-            textGuest = ""
-            print("Список гостей: ")
-            for i in range(0,len(guestList)): #создаем цикл для просмотра списка гостей
-                textGuest += f"{i + 1} - {guestList[i]}\n" # плюсуем к i единицу для того чтобы в выведенном списке отсчет начинался от 1, а не от 0
-            print(textGuest) # вывод списка гостей если он не пуст
-        else:
-            print("Список пуст!")
-    elif vybor == "4":
-        if 5 < len(guestList) < 10: # заканчиваем цикл по запросу, если число гостей больше 5 и меньше 10
-            break
-
 import json
 
 
 def globalReg():
-    if len(guestList) <= 5: # если гостей до 5 включительно задаем три действия
+    base_list = open("base.json","r",encoding="utf-8")
+    base_list_read = base_list.read()
+    new_base = json.loads(base_list_read) # из строки сделали массив
+    base_list_read.close()
+    if len(base_list) <= 5: # если гостей до 5 включительно задаем три действия
         vybor = int(input("Выберите действие\n1 - Добавить гостя\n2 - Удалить гостя\n3 - Просмотреть список гостей\n> "))
     else: # если гостей больше 5 задаем четыре действия
         vybor = int(input("Выберите действие\n1 - Добавить гостя\n2 - Удалить гостя\n3 - Просмотреть список гостей\n4 - Закончить приглашение?\n> "))
@@ -48,12 +16,19 @@ def globalReg():
         guestDel()
     elif vybor == 3:
         listShow()
-    else:
-        break
+globalReg()
 
 def guestAdd():
-    if len(guestList) < 10: # максимальное количество гостей 10
+    base_list = open("base.json","r",encoding="utf-8")
+    base_list_read = base_list.read()
+    new_base = json.loads(base_list_read) # из строки сделали массив
+    base_list_read.close()
+    if len(base_list) < 10: # максимальное количество гостей 10
         guest_name = input("Введите имя: ")
+        blackList = open("blackList.json","r",encoding="utf-8")
+        blackList_read = blackList.read()
+        new_blackList = json.loads(blackList_read)
+        blackList_read.close()
         if guest_name not in blackList: # если введенное имя не присутствует в черном списке, то добавляем его в список гостей
             base_list = open("base.json","r",encoding="utf-8")
             base_list_read = base_list.read()
@@ -61,27 +36,54 @@ def guestAdd():
             new_base.append({
                     "guest_name" : guest_name
             }) # добавление введенного имени в список гостей
+            base_list = json.dumps(base_list, ensure_ascii=False)
+            base_list_write = open("base.json","w",encoding="utf-8")
+            base_list_write.write(base_list)
+            base_list_write.close()
         else: # в противном случай имя не добавляется
             print("Данный гость в черном списке!")
     else:
         print("Список гостей заполнен!")
+guestAdd()
 
 def guestDel():
-    if len(guestList) > 0:
+    if len(base_list) > 0:
         guest_name = input("Введите имя: ")
+        blackList = open("blackList.json","r",encoding="utf-8")
+        blacklist_read = black_list.read()
+        new_blackList = json.loads(blackList_read)
+        new_blackList.append({
+                "guest_name" : guest_name
+        })
+        blackList = json.dumps(blackList, ensure_ascii=False)
+        blackList_write = open("blackList.json","w",encoding="utf-8")
+        blackList_write.write(blackList)
+        blackList_write.close()
         base_list = open("base.json","r",encoding="utf-8")
         base_list_read = base_list.read()
-        new_base = json.loads(base_list_read)
+        new_base = json.loads(base_list_read) # из строки сделали массив
+        new_base.remove(guest_name) # добавление введенного имени в список гостей
+        base_list = json.dumps(base_list, ensure_ascii=False)
+        base_list_write = open("base.json","w",encoding="utf-8")
+        base_list_write.write(base_list)
+        base_list_write.close()
+
         new_base.remove(guest_name) # удаление введенного имени из списка гостей если список не пуст
     else:
         print("Список пуст!")
+guestDel()
 
 def listShow():
-    if len(guestList) > 0:
-            textGuest = ""
-            print("Список гостей: ")
-            for i in range(0,len(guestList)): #создаем цикл для просмотра списка гостей
-                textGuest += f"{i + 1} - {guestList[i]}\n" # плюсуем к i единицу для того чтобы в выведенном списке отсчет начинался от 1, а не от 0
+    if len(base_list) > 0:
+        textGuest = ""
+        print("Список гостей: ")
+        base_list = open("base.json","r",encoding="utf-8")
+        base_list_read = base_list.read()
+        new_base = json.loads(base_list_read) # из строки сделали массив
+        base_list_read.close()
+        for i in range(0,len(base_list)): #создаем цикл для просмотра списка гостей
+            textGuest += f"{i + 1} - {guestList[i]}\n" # плюсуем к i единицу для того чтобы в выведенном списке отсчет начинался от 1, а не от 0
             print(textGuest) # вывод списка гостей если он не пуст
     else:
         print("Список пуст!")
+listShow()
--- MyProject/main.py
+++ MyProject/main.py
@@ -3,7 +3,7 @@ import json
 
 def reg():
     store_list_read = open("./store/store.json","r",encoding="utf-8") # читаем файл
-    store_list = json.loads(store_list_read.read()) # из строки делаем массив
+    store_list = json.loads(store_list_read) # из строки делаем массив
     store_list_read.close()
     print(store_list)
     regs = Registration()
