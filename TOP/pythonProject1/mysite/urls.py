2985e55e6aae1967a5eb84d4498c316f9139fe09
Ринат Мухитов <malayka_73@inbox.ru>
2023-08-29 21:22:30 +0400

    Django

--- TOP/pythonProject1/mysite/urls.py
+++ TOP/pythonProject1/mysite/urls.py
@@ -15,11 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.urls import path, include
 from django.urls import path
+from django.conf.urls.static import static # импортировали из настроек статику
+from django.conf import settings # импортировали из натсроек свои записи
 
-import blog.views
+import home.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', blog.views.blogView, name='blog'),
+    path('', home.views.homeView, name='home'),
+    path('blog/', include('blog.urls'), name = 'blog'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
