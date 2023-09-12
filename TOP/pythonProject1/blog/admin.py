d86df59e7dcb66a891a29f646d7c10fadb636296
Ринат Мухитов <malayka_73@inbox.ru>
2023-09-12 21:11:36 +0400

    pythonProject1

--- TOP/pythonProject1/blog/admin.py
+++ TOP/pythonProject1/blog/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Article
 
-admin.site.register(Article)
+# admin.site.register(Article)
+@admin.register(Article)
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date')
 
--- TOP/pythonProject1/blog/models.py
+++ TOP/pythonProject1/blog/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
 class Article(models.Model):
-    title = models.CharField(max_length=50)  # Строка с ограниченными знаками
-    desc = models.CharField(max_length=150)  # Строка с ограниченными знаками
-    image = models.ImageField(upload_to='blog/image/')  # Строка под изображение
-    date = models.DateField()  # Дата
-    url = models.URLField()  # Ссылка
+    title = models.CharField('Заголовок', max_length=50)  # Строка с ограниченными знаками
+    desc = models.TextField('Описание')  # Строка с ограниченными знаками
+    image = models.ImageField('Изображение', upload_to='blog/image/')  # Строка под изображение
+    date = models.DateField('Дата')  # Дата
+    url = models.URLField('Доп.источник', blank=False)  # Ссылка
+
+    def __str__(self):
+        return f"{self.title} | {self.date}"  # это для красивого отображения в админке
