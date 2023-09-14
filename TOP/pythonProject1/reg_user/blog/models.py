2fed39eb49065c9e9146a4dff3f42f007f56c773
Ринат Мухитов <malayka_73@inbox.ru>
2023-09-14 21:14:56 +0400

    pythonProject2

--- TOP/pythonProject1/reg_user/blog/models.py
+++ TOP/pythonProject1/reg_user/blog/models.py
@@ -5,7 +5,7 @@ class Article(models.Model):
     desc = models.TextField('Описание')  # Строка с ограниченными знаками
     image = models.ImageField('Изображение', upload_to='blog/image/')  # Строка под изображение
     date = models.DateField('Дата')  # Дата
-    url = models.URLField('Доп.источник', blank=False)  # Ссылка
+    url = models.URLField('Доп.источник', blank=True)  # Ссылка
 
     def __str__(self):
         return f"{self.title} | {self.date}"  # это для красивого отображения в админке
