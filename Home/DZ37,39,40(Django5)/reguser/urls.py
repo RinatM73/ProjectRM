3353bc00965afce92620da4b2c423665600295c0
Ринат Мухитов <malayka_73@inbox.ru>
2023-09-20 23:37:19 +0400

    DZ

--- Home/DZ37,39,40(Django5)/reguser/urls.py
+++ Home/DZ37,39,40(Django5)/reguser/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('register/', reguserview, name = 'reguser')
+    path('register/', reguserview, name = 'reguser'),
+    path('login/', loginuserview, name = 'loginuser'),
+    path('logout/', logoutuserview, name = 'logoutuser'),
+    path('profile/', profileview, name = 'profile'),
+    path('profileup/', profileupview, name = 'profileup'),
 ]
